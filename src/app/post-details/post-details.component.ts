7e1b123b16c2f0c40822f79aa96f0ba5ca40b3d7
Thomas <thomasmrt200@gmail.com>
2021-11-19 01:13:08 +0100

    up

--- src/app/post-details/post-details.component.ts
+++ src/app/post-details/post-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../services/post.service';
+import { Post } from '../models/post.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post-details',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostDetailsComponent implements OnInit {
 
-  constructor() { }
+
+  postFocus: Post = {
+  title: '',
+  description: '',
+  published: false,
+}
+
+  constructor(private postService: PostService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getPost(this.route.snapshot.params.id);
   }
 
+  getPost(id: string): void {
+    this.postService.get(id)
+      .subscribe(
+        data => {
+          this.postFocus = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+    }
 }
