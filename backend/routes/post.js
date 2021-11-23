ce76fdbbc22935e234110c4612935573d259b6af
Thomas <thomasmrt200@gmail.com>
2021-11-23 09:45:55 +0100

    up

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const postCtrl = require('../controllers/post');
 
-router.get('/', auth, postCtrl.getAllPost);
-router.post('/', auth, postCtrl.createPost);
-router.get('/:id', auth, postCtrl.getOnePost);
-router.put('/:id', auth, postCtrl.modifyPost);
-router.delete('/:id', auth, postCtrl.deletePost);
+router.get('/',  postCtrl.getAllPost);
+router.post('/',  postCtrl.createPost);
+router.get('/:id',  postCtrl.getOnePost);
+router.put('/:id',  postCtrl.modifyPost);
+router.delete('/:id',  postCtrl.deletePost);
 // router.post('/:id/like', postCtrl.manageLikePost);
 
 module.exports = router;
